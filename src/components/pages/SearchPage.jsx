4b0ee5c1ed43e00a85e039e7e835a964347eda76
yawasJph <josephyakuash@gmail.com>
2025-12-23 23:38:17 -0500

    searchPage fix loyout v13

--- src/components/pages/SearchPage.jsx
+++ src/components/pages/SearchPage.jsx
@@ -88,7 +88,7 @@ const SearchPage = () => {
               </h3>
               <TrendingTopics />
             </div>
-            <div className="min-w-full">
+            <div className="max-w-[400px] sm:max-w-full">
               <h3 className="text-lg font-bold dark:text-white mb-4 px-2">
                 Personas recomendadas
               </h3>
