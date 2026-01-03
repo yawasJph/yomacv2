ae5f7eb3574c8271da91c35a8e32867b1e154d90
yawasJph <josephyakuash@gmail.com>
2026-01-03 16:10:22 -0500

    add push notif v3

--- src/hooks/usePushNotifications.js
+++ src/hooks/usePushNotifications.js
@@ -8,38 +8,40 @@ export const usePushNotifications = () => {
   const subscribeToPush = async () => {
     console.log("1. Iniciando suscripción...");
     try {
-      if (!('serviceWorker' in navigator)) {
+      if (!("serviceWorker" in navigator)) {
         throw new Error("Service Worker no soportado");
       }
-      
+
       const registration = await navigator.serviceWorker.ready;
       console.log("2. Service Worker listo.");
-      
+
       // Verifica si ya hay una suscripción activa
       let subscription = await registration.pushManager.getSubscription();
-      
+
       if (!subscription) {
+        // CONVIERTE LA KEY AQUÍ
+        const convertedVapidKey = urlBase64ToUint8Array(
+          "BCQUCSKlyz9Eux_zMFAyUYGr-TY7Pfqm_IW3l1sOS9zZJsum8IPYR0UtwBj01rIlp0bsUPR1a_FJ7Srl1f9gWKU"
+        );
+
         subscription = await registration.pushManager.subscribe({
           userVisibleOnly: true,
-          applicationServerKey: 'BCQUCSKlyz9Eux_zMFAyUYGr-TY7Pfqm_IW3l1sOS9zZJsum8IPYR0UtwBj01rIlp0bsUPR1a_FJ7Srl1f9gWKU'
+          applicationServerKey: convertedVapidKey, // Usa la llave convertida
         });
       }
-      console.log("3. Suscripción obtenida del navegador.");
+      console.log("3. Suscripción obtenida:", subscription);
 
-      const { error } = await supabaseClient
-        .from('push_subscriptions')
-        .insert({
-          user_id: user.id,
-          subscription: subscription.toJSON()
-        });
+      const { error } = await supabaseClient.from("push_subscriptions").insert({
+        user_id: user.id,
+        subscription: subscription.toJSON(),
+      });
 
       if (error) throw error;
       return true;
-
     } catch (error) {
       console.error("Error detallado:", error);
       // Esto te ayudará a ver el error real en el móvil si no tienes inspector
-      // alert("Error: " + error.message); 
+      // alert("Error: " + error.message);
       throw error;
     }
   };
