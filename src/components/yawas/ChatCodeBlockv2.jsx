458d50d44fa2ad137eff31a81de6139619eae9f5
yawasJph <josephyakuash@gmail.com>
2026-02-15 20:17:17 -0500

    texting code blosk yawas v3

--- src/components/yawas/ChatCodeBlockv2.jsx
+++ src/components/yawas/ChatCodeBlockv2.jsx
@@ -29,7 +29,7 @@ export function ChatCodeBlock({ code, language }) {
   };
 
   return (
-    <div className="group relative my-4 overflow-hidden rounded-2xl border border-neutral-800 bg-neutral-900 shadow-xl max-w-95">
+    <div className="group relative my-4 overflow-hidden rounded-2xl border border-neutral-800 bg-neutral-900 shadow-xl max-w-100">
 
       {/* HEADER */}
       <div className="flex items-center justify-between px-4 py-2 bg-neutral-800/90 backdrop-blur border-b border-neutral-700">
