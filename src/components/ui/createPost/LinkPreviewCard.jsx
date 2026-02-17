23da4bbee5ab1ab0135fa1fcdc73a6f8ecef14f7
yawasJph <josephyakuash@gmail.com>
2026-02-17 17:42:49 -0500

    og card testing --d- v3

--- src/components/ui/createPost/LinkPreviewCard.jsx
+++ src/components/ui/createPost/LinkPreviewCard.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { X } from "lucide-react";
 
 const LinkPreviewCard = ({ preview, isLoading, onClose }) => {
-
   if (isLoading) {
     return (
       <div className="mt-4 p-3 border border-gray-100 dark:border-neutral-800 rounded-xl flex items-center gap-3 animate-pulse">
@@ -34,19 +33,24 @@ const LinkPreviewCard = ({ preview, isLoading, onClose }) => {
         href={preview.url}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex w-full bg-white dark:bg-neutral-900 border border-gray-200 dark:border-neutral-700 rounded-xl overflow-hidden shadow-sm hover:shadow-md transition-all duration-200 mb-4"
+        // className="flex w-full bg-white dark:bg-neutral-900 border border-gray-200 dark:border-neutral-700 rounded-xl overflow-hidden shadow-sm hover:shadow-md transition-all duration-200 mb-4"
+        className="group flex w-full bg-white dark:bg-neutral-900 border border-gray-200 dark:border-neutral-800 rounded-xl hover:bg-gray-50 dark:hover:bg-neutral-800/40 transition-all duration-200 mb-3 no-underline shadow-sm active:scale-[0.99] overflow-hidden"
       >
         {preview.image && (
-          <div className="w-30 md:w-44 min-h-24 shrink-0 bg-gray-200 dark:bg-neutral-800 overflow-hidden">
+          <div
+            //className="w-30 md:w-44 min-h-24 shrink-0 bg-gray-200 dark:bg-neutral-800 overflow-hidden"
+            className="relative w-28 sm:w-36 aspect-4/3 shrink-0 bg-gray-100 dark:bg-neutral-800 overflow-hidden"
+          >
             <img
               src={preview.image}
               alt="Preview"
-              className="w-full h-full object-cover"
+              //className="w-full h-full object-cover"
+              className="w-full h-full object-cover transition-all duration-500 group-hover:scale-105"
             />
           </div>
         )}
 
-        <div className="flex-1 p-3 flex flex-col justify-center">
+        {/* <div className="flex-1 p-3 flex flex-col justify-center">
           {preview.publisher && (
             <p className="text-[10px] font-medium text-blue-600 dark:text-blue-400 uppercase tracking-wide mb-1 line-clamp-1">
               {preview.publisher}
@@ -75,6 +79,42 @@ const LinkPreviewCard = ({ preview, isLoading, onClose }) => {
               </span>
             </div>
           )}
+        </div> */}
+        <div className="flex flex-col flex-1 min-w-0 px-3 py-2">
+          <div className="flex-1 min-w-0">
+            {preview.publisher && (
+              <span className="block text-[9px] font-semibold text-blue-600 dark:text-blue-400 uppercase tracking-wide mb-0.5 truncate">
+                {preview.publisher}
+              </span>
+            )}
+
+            {preview.title && (
+              <h3 className="text-[13px] sm:text-sm font-semibold text-gray-900 dark:text-gray-100 leading-snug line-clamp-2">
+                {preview.title}
+              </h3>
+            )}
+
+            {preview.description && (
+              <p className="text-gray-500 dark:text-gray-400 text-[11px] sm:text-xs line-clamp-2 mt-1 leading-snug">
+                {preview.description}
+              </p>
+            )}
+          </div>
+
+          {/* FOOTER */}
+          <div className="flex items-center gap-1.5 mt-1.5">
+            <img
+              src={preview.logo || favicon}
+              alt="Prevciew"
+              loading="lazy"
+              decoding="async"
+              className="w-3 h-3 rounded-sm object-contain"
+            />
+
+            <span className="text-[10px] font-medium text-gray-400 dark:text-neutral-500 truncate">
+               {new URL(preview.url).hostname}
+            </span>
+          </div>
         </div>
       </a>
     </div>
